c1a8b8f86da4ae53b3e1e72ee74b672caa7f5c64
Fabíola Gonçalves Silva <fabiolags449@gmail.com>
2015-05-29 20:25:43 -0300

    Update calculadora.js

--- test/calculadora.js
+++ test/calculadora.js
@@ -22,6 +22,15 @@ describe('Calculadora', function() {
 				resultado.should.be.a.Number;
 				resultado.should.be.equal(-3);
 			});
+			it('Deveria retornar 5 quando for passado 1 e 4.', function() {
+				var numero1 = 1;
+				var numero2 = 4;
+				
+				var resultado = controller.utils.calculos.somar(numero1, numero2);
+				
+				resultado.should.be.a.Number;
+				resultado.should.be.equal(-3);
+			});
 			
 			it('Deveria retornar 3 quando for passado -1 e 4.', function() {
 				var numero1 = -1;
